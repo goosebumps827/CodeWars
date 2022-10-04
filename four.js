330315cf300e87d3b65df44bee10198d313bd4d1
Tyler Neumann <77752264+goosebumps827@users.noreply.github.com>
2022-10-04 15:57:03 -0400

    Create four.js

---
+++ four.js
@@ -0,0 +1,6 @@
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+var countBits = function(n) {
+    return n.toString(2).split('').filter(x=> x == '1' ? true:false).length
+    
+  };
